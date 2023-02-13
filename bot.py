7612007f4ebdcc54275994c4ee813ae9ac655959
Bruno Braga <brunobragast@gmail.com>
2023-02-13 01:01:58 -0300

    Can now cancel an ongoing farm

--- bot.py
+++ bot.py
@@ -1,54 +1,70 @@
 import win32com.client as comctl
 import time
 
-wsh = comctl.Dispatch("WScript.Shell")
 class TwBot:
-    def __init__(self):
+    def __init__(self, window):
         self.stop_iteration = False
+        self.wsh = comctl.Dispatch("WScript.Shell")
+        self.running = True
+        self.window = window
+    
+    def start(self):
+       self.running = True
         
+    def stop(self):
+        self.running = False
+    
     # Google Chrome window title
-    def farmer(self):
-        wsh.AppActivate("br117.tribalwars.com.br/")
-        countdown()
-        refresh_page()
-
-        wsh.SendKeys("{8}")
+    def prepare_farm(self):
+        self.prepare()
+        self.wsh.SendKeys("{8}")
         time.sleep(2)
-        wsh.SendKeys("{9}")
+        self.wsh.SendKeys("{9}")
         time.sleep(2)
-        
-        for i in range(500):
-            wsh.SendKeys("{m}")
-            time.sleep(0.22)
+    
+    def farm(self, counter):
+        if self.running:
+            self.wsh.SendKeys("{m}")
+        if counter >= 1000:
+            self.stop()
+        counter += 1
+        self.window.after(225, lambda : self.farm(counter))
+    
+    def prepare_scavange(self):
+        self.prepare()
     
     def scavanger(self):
-        countdown()
-        refresh_page()
-
-        for i in range(7):
-            for j in range(4):
-                wsh.SendKeys("{6}")
-                time.sleep(1)
-                wsh.SendKeys("{7}")
-                time.sleep(2)
-                wsh.SendKeys("{ENTER}", 0)
-                time.sleep(2)
-            wsh.SendKeys("{d}")
-            time.sleep(3)
+        for j in range(4):
+            self.wsh.SendKeys("{6}")
+            time.sleep(1)
+            self.wsh.SendKeys("{7}")
+            time.sleep(2)
+            self.wsh.SendKeys("{ENTER}", 0)
+            time.sleep(2)
+        self.wsh.SendKeys("{d}")
+        time.sleep(5)
             
     def full(self):
-        self.farmer()
+        self.farm()
         time.sleep(5)
         self.scavanger()
+        
+    def cancel_action(self):
+        if self.stop_iteration is False:
+            self.stop_iteration = True
+    
+    def prepare(self):
+        self.countdown()
+        self.refresh_page()
+        self.start()
+        
+    def countdown(self):
+        for i in range(5):
+            print(f"Starting in {5 - i}...")
+            time.sleep(1)
 
-            
-def countdown():
-    for i in range(5):
-        print(f"Starting in {5 - i}...")
-        time.sleep(1)
-
-def refresh_page():
-    wsh.SendKeys("{F5}")
-    time.sleep(2)
+    def refresh_page(self):
+        self.wsh.SendKeys("{F5}")
+        time.sleep(2)
     
         
--- runner.py
+++ runner.py
@@ -2,32 +2,63 @@ import tkinter as tk
 from bot import TwBot
 
 window = tk.Tk()
-stop_iteration = False
-twBot = TwBot() 
+twBot = TwBot(window) 
+running = True
 
+def start():
+   global running
+   running = True
+
+def stop():
+   global running
+   running = False
+    
+def scavange(counter):
+    if running:
+        twBot.scavanger()
+    if counter >= 7:
+        stop()
+    window.after(225, scavange)
+    
 farm_button = tk.Button(text="Farm",
-    width=10,
-    height=5,
+    width=30,
+    height=2,
     bg="blue",
     fg="yellow",
-    command=twBot.farmer)
+    command=lambda : {
+        twBot.prepare_farm(),
+        twBot.farm(0)
+        })
 farm_button.pack()
 
 scavange_button = tk.Button(text="Scavange",
-    width=10,
-    height=5,
+    width=30,
+    height=2,
     bg="green",
     fg="yellow",
-    command=twBot.scavanger)
+    command=lambda : {
+        start(),
+        twBot.prepare_scavange(),
+        scavange(0)
+        })
 scavange_button.pack()
 
 full_button = tk.Button(text="Full",
-    width=10,
-    height=5,
+    width=30,
+    height=2,
     bg="purple",
     fg="yellow",
     command=twBot.full)
 full_button.pack()
 
+cancel_button = tk.Button(text="Cancel",
+    width=30,
+    height=2,
+    bg="red",
+    fg="yellow",
+    command=twBot.stop)
+cancel_button.pack()
 
 window.mainloop()
+
+
